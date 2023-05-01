e143bae9dbd8d06ea6e4bc3085207ad613262450
Simon Emms <simon@simonemms.com>
2023-05-01 16:46:40 +0000

    feat(layout): configure main layout

---
+++ src/lib/components/page-header.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  export let title: string = 'Untitled Page';
+</script>
+
+<svelte:head>
+  <title>Simon Emms | {title}</title>
+</svelte:head>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,57 @@
-<script>
+<script lang="ts">
+  import Navbar from '../lib/components/navbar.svelte';
+  import PageHeader from '../lib/components/page-header.svelte';
   import '../app.scss';
+
+  const subtitle = 'Software Engineer, Technical Leader, Solutions Designer';
+  const currentYear = new Date().getUTCFullYear();
 </script>
 
-<slot />
+<PageHeader title={subtitle} />
+
+<section class="hero is-large">
+  <div class="hero-head">
+    <Navbar />
+  </div>
+
+  <div class="hero-body has-text-weight-light has-text-white">
+    <p class="is-size-1 is-size-3-mobile">Simon Emms</p>
+    <p class="is-size-5 is-size-6-mobile">{subtitle}</p>
+  </div>
+</section>
+
+<div class="site-content">
+  <slot />
+</div>
+
+<footer class="footer has-background-black-ter has-text-white has-text-centered">
+  <div class="content">
+    <div>@todo</div>
+    <hr class="has-background-grey-dark" />
+    <div class="is-size-7">
+      &copy; {currentYear}
+      <span class="px-2">|</span>
+      Simon Emms
+    </div>
+  </div>
+</footer>
+
+<style lang="scss">
+  .hero {
+    background: {
+      image: url('/img/devopsdays.jpg');
+      position: center center;
+      size: cover;
+    }
+  }
+
+  .site-content {
+    flex: 1;
+  }
+
+  .footer {
+    hr {
+      height: 1px;
+    }
+  }
+</style>
