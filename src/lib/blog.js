c4e3da14e7a614b270415351a737dc28883fd36a
Simon Emms <simon@simonemms.com>
2023-05-01 21:32:49 +0000

    feat(blog): configure the blog post list page

---
+++ src/lib/blog.js
@@ -0,0 +1,74 @@
+import fs from 'fs';
+import { sync as glob } from 'glob';
+import frontMatter from 'front-matter';
+
+/**
+ * @typedef {{post: string, route: string, body: string, attributes: any}} Post
+ */
+
+/**
+ * @returns {string[]}
+ */
+export function getEntries() {
+  return listPosts().map(({ route }) => route);
+}
+
+/**
+ * @returns {Post[]}
+ */
+export function listPosts() {
+  return glob('./src/blog/**/*.md')
+    .map((post) => parsePost(post))
+    .filter((post) => post !== null);
+}
+
+/**
+ * @param  {string} post
+ * @returns {Post | null}
+ */
+export function parsePost(post) {
+  const regex = /(_)?(\d{4})-(\d{2})-(\d{2})-(.*).md/;
+
+  if (regex.test(post)) {
+    const matches = post.match(regex);
+    if (matches !== null) {
+      const [, hidden, year, month, day, title] = matches;
+
+      const route = ['', 'blog', year, month, day, title].join('/');
+
+      const data = fs.readFileSync(post, 'utf8');
+      const payload = frontMatter(data);
+
+      const { attributes, body } = payload;
+      attributes.hidden = !!hidden;
+      attributes.keys = {
+        year: parseInt(year, 10),
+        month: parseInt(month, 10),
+        day: parseInt(day, 10),
+        title,
+      };
+      if (!attributes.date) {
+        attributes.date = new Date(
+          attributes.keys.year,
+          attributes.keys.month - 1,
+          attributes.keys.day,
+        );
+      }
+
+      /** @type {string[]} */
+      const tags = [];
+      if (attributes.tags) {
+        tags.push(...attributes.tags.split(','));
+      }
+      attributes.tags = tags.map((tag) => tag.trim()).sort();
+
+      return {
+        post,
+        route,
+        body,
+        attributes,
+      };
+    }
+  }
+  return null;
+}
---
+++ src/lib/components/date-format.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { DateTime } from 'luxon';
+
+  export let date: Date;
+
+  export let format: Intl.DateTimeFormatOptions = DateTime.DATETIME_SHORT;
+</script>
+
+<time datetime={date.toISOString()}>
+  {DateTime.fromJSDate(date).toLocaleString(format)}
+</time>
