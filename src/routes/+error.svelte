badd908e2ff0a3425c3c3629e7e234f6c157b618
Simon Emms <simon@simonemms.com>
2023-05-06 15:54:12 +0000

    feat(error): make an error page with a custom layout

---
+++ src/routes/+error.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { page } from '$app/stores';
+  import PageHeader from '$lib/components/page-header.svelte';
+  import PageTitle from '$lib/components/page-title.svelte';
+</script>
+
+<PageHeader title={$page.error.message} />
+
+<section class="hero is-fullheight">
+  <div class="hero-body is-justify-content-center">
+    <div class="has-background-danger has-text-white p-3 has-text-centered">
+      <PageTitle addPadding={false}>There's been an error</PageTitle>
+
+      <div class=" content">
+        <p class="is-size-4 has-text-weight-light">{$page.error.message}</p>
+
+        <p>
+          The best thing you can do now is go back to the
+          <a href="/" class="is-link">homepage</a>
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<style lang="scss">
+  .hero {
+    background: {
+      image: url('/img/error.jpg');
+      position: center center;
+      size: cover;
+    }
+  }
+</style>
