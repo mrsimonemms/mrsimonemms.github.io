29655c6d2b3b7efb76a24394af0de01a2c1edf50
Simon Emms <simon@simonemms.com>
2023-05-06 15:54:12 +0000

    feat(error): make an error page with a custom layout

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+  import Navbar from '$lib/components/navbar.svelte';
+  import { cv } from '$lib/stores/cv';
+
+  const subtitle = 'Software Engineer, Technical Leader, Solutions Designer';
+  const currentYear = new Date().getUTCFullYear();
+</script>
+
+<section class="hero is-large">
+  <div class="hero-head">
+    <Navbar />
+  </div>
+
+  <div class="hero-body has-text-weight-light has-text-white">
+    <p class="is-size-1 is-size-3-mobile">Simon Emms</p>
+    <p class="is-size-5 is-size-6-mobile">{subtitle}</p>
+  </div>
+</section>
+
+<div class="site-content">
+  <slot />
+</div>
+
+<footer class="footer has-background-black-ter has-text-white has-text-centered">
+  <div class="content">
+    {#each cv.basics.profiles as profile}
+      <a
+        class="has-tooltip-arrow has-tooltip-bottom"
+        href={profile.url}
+        target="_blank"
+        data-tooltip={profile.network}
+      >
+        <span class="icon is-large has-text-white mx-3">
+          <i class="mdi mdi-24px mdi-{profile.network.toLowerCase()}" />
+        </span>
+      </a>
+    {/each}
+    <hr class="has-background-grey-dark" />
+    <div class="is-size-7">
+      &copy; {currentYear}
+      <span class="px-2">|</span>
+      Simon Emms
+    </div>
+  </div>
+</footer>
+
+<style lang="scss">
+  .hero {
+    background: {
+      image: url('/img/devopsdays.jpg');
+      position: center center;
+      size: cover;
+    }
+  }
+
+  .site-content {
+    flex: 1;
+  }
+
+  .footer {
+    hr {
+      height: 1px;
+    }
+  }
+</style>
---
+++ src/routes/+error.svelte
@@ -0,0 +1,36 @@
+<script>
+  import { page } from '$app/stores';
+  import PageHeader from '$lib/components/page-header.svelte';
+  import PageTitle from '$lib/components/page-title.svelte';
+
+  const errorMessage = $page.error?.message ?? 'Unknown error';
+</script>
+
+<PageHeader title={errorMessage} />
+
+<section class="hero is-fullheight">
+  <div class="hero-body is-justify-content-center">
+    <div class="has-background-danger has-text-white p-3 has-text-centered">
+      <PageTitle addPadding={false}>There's been an error</PageTitle>
+
+      <div class=" content">
+        <p class="is-size-4 has-text-weight-light">{errorMessage}</p>
+
+        <p>
+          The best thing you can do now is go back to the
+          <a href="/" class="is-link">homepage</a>
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<style lang="scss">
+  .hero {
+    background: {
+      image: url('/img/error.jpg');
+      position: center center;
+      size: cover;
+    }
+  }
+</style>
